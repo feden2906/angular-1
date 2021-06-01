162b5376220bb89a10b2da69571d85ee6f546f03
NataWhite <nata.ivanova.one@gmail.com>
2021-06-01 23:12:54 +0300

    router

--- src/app/components/comment-details/comment-details.component.ts
+++ src/app/components/comment-details/comment-details.component.ts
@@ -1,4 +1,7 @@
 import {Component,} from '@angular/core';
+import {IComment} from "../../models/Comment";
+import {ActivatedRoute} from "@angular/router";
+import {CommentService} from "../../services/comment.service";
 
 @Component({
   selector: 'app-comment-details',
@@ -7,6 +10,13 @@ import {Component,} from '@angular/core';
 })
 export class CommentDetailsComponent {
 
+  singleComment: IComment;
+
+  constructor(private activatedRoute: ActivatedRoute, private commentService: CommentService) {
+    this.activatedRoute.params.subscribe(params => {
+      this.commentService.getComment(params.id).subscribe(value => this.singleComment = value)
+    })
+  }
 
 
 }
--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,28 +1,22 @@
 import {Component, Input} from '@angular/core';
 import {IComment} from "../../models/Comment";
 import {ActivatedRoute, Router} from "@angular/router";
-import {CommentService} from "../../services/comment.service";
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css']
 })
-export class CommentComponent{
+export class CommentComponent {
 
   @Input()
-  comment : IComment;
+  comment: IComment;
 
-  singleComment: IComment;
-
-  constructor(private activatedRoute: ActivatedRoute, private commentService: CommentService) {
-
-  }
-  openDetails(): void {
-    this.activatedRoute.params.subscribe(params => {
-      this.commentService.getComment(params.id).subscribe(data => this.singleComment = data)
-    })
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
   }
 
+  openComment(): void {
+    this.router.navigate([this.comment.id], {relativeTo: this.activatedRoute});
+  }
 
 }
